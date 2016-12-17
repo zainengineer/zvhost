ac393d41185e2f8ad5e14b7e8a41a344e849c46e
Zain <zainengineer@gmail.com>
2016-12-17 12:00:07 +1030

    format: auto format by phpstorm

--- js/app/db_app.js
+++ js/app/db_app.js
@@ -1,11 +1,11 @@
 var templateHtml;
 var commandTemplateHtml;
-jQuery.get('template/db.txt', function (data){
-        templateHtml = data;
-        startTemplate(data,db_param);
+jQuery.get('template/db.txt', function (data) {
+    templateHtml = data;
+    startTemplate(data, db_param);
 });
 
-function startTemplate(html){
+function startTemplate(html) {
     jQuery('#template').html(html);
     html = Mustache.to_html(html, db_param);
     jQuery('#rendered').html(html);
@@ -15,18 +15,17 @@ function startTemplate(html){
         theme: 'bootstrap3',
         iconlib: 'fontawesome4'
     };
-    
-    window.editor = new JSONEditor(container,options);
+
+    window.editor = new JSONEditor(container, options);
     editor.setValue(db_param);
-    
-    editor.on('change', function(){
+
+    editor.on('change', function () {
         var json = editor.getValue();
-        renderMustache(templateHtml,json);
+        renderMustache(templateHtml, json);
     });
 }
 
-function renderMustache(templateHtml, json)
-{
+function renderMustache(templateHtml, json) {
     var html = Mustache.to_html(templateHtml, json);
     jQuery('#rendered').html(html);
 }
--- js/app/vhost_app.js
+++ js/app/vhost_app.js
@@ -1,18 +1,18 @@
 var templateHtml;
 var commandTemplateHtml;
-jQuery.get('template/version1.conf', function (data){
-        templateHtml = data;
-        startTemplate(data,param);
+jQuery.get('template/version1.conf', function (data) {
+    templateHtml = data;
+    startTemplate(data, param);
 });
 
-jQuery.get('template/bash.txt', function (data){
-        commandTemplateHtml = data;
+jQuery.get('template/bash.txt', function (data) {
+    commandTemplateHtml = data;
 });
 
 
 var container = document.getElementById("jsoneditor");
 
-function startTemplate(html){
+function startTemplate(html) {
     jQuery('#template').html(html);
     var html = Mustache.to_html(html, param);
     jQuery('#rendered').html(html);
@@ -22,31 +22,29 @@ function startTemplate(html){
         theme: 'bootstrap3',
         iconlib: 'fontawesome4'
     };
-    
-    window.editor = new JSONEditor(container,options);
+
+    window.editor = new JSONEditor(container, options);
     editor.setValue(param);
-    
-    editor.on('change', function(){
+
+    editor.on('change', function () {
         var json = editor.getValue();
-        renderMustache(templateHtml,json);
+        renderMustache(templateHtml, json);
     });
 }
 
-function renderMustache(templateHtml, json)
-{
+function renderMustache(templateHtml, json) {
     var html = Mustache.to_html(templateHtml, json);
     jQuery('#rendered').html(html);
     json.vhost = html;
     renderCommand(json);
 }
 
-function renderCommand(json)
-{
-    if (!templateHtml || !commandTemplateHtml){
+function renderCommand(json) {
+    if (!templateHtml || !commandTemplateHtml) {
         return;
     }
     var html = Mustache.to_html(commandTemplateHtml, json);
     html = String(html).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
     jQuery('#command').html(html);
-    
+
 }
