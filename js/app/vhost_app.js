1c96a9f85fbddcb71d1a9a3ffb0ce8f97a7d9dcd
Zain ul abidin <zain@aligent.com.au>
2017-01-24 15:26:16 +1030

    using replaceall in string

--- js/app/vhost_app.js
+++ js/app/vhost_app.js
@@ -1,3 +1,7 @@
+String.prototype.replaceAll = function(str1, str2, ignore)
+{
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+}
 var templateHtml;
 var commandTemplateHtml;
 var VHostAppVars = {};
@@ -60,8 +64,8 @@ function postProcess(param)
             objectData =  JSON.parse(objectData);
         }
         if (objectData.apache == 'httpd'){
-            output = output.replace('/etc/httpd/sites-available','/etc/httpd/conf.d')
-            output = output.replace('sudo a2ensite','#sudo a2ensite');
+            output = output.replaceAll('/etc/httpd/sites-available','/etc/httpd/conf.d')
+            output = output.replaceAll('sudo a2ensite','#sudo a2ensite');
         }
         var html = String(output).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
         jQuery('#command').html(html);
