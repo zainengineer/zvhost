33ce80c4b700259a81a775f824c6662a0e1b4eba
Zain ul abidin <zain@aligent.com.au>
2018-03-21 10:06:09 +1030

    render after 1 second

--- js/app/param.js
+++ js/app/param.js
@@ -2,7 +2,14 @@ let param = {
     domain: 'www.vss.com',
     location: '/var/www/vhosts/vss',
     apache: 'apache2',
-    aliases : '',
+    aliases: '',
     autoprepend: false
 };
 window.param = $.extend(param, ZStorage.getObject('vhost_param'));
+$(function () {
+    window.setTimeout(function () {
+        if (window.editor) {
+            window.editor.trigger('change')
+        }
+    }, 1000);
+});
